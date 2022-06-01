b69a4a0559155807feeed263aff7293131f71b1f
javierpb275 <javierpb275@gmail.com>
2022-06-01 20:07:29 +0200

    sign up and write users to a db

--- create_table.py
+++ create_table.py
@@ -1,27 +1,11 @@
 import sqlite3
 
 connection = sqlite3.connect('data.db')
-
 cursor = connection.cursor()
 
-create_table = "CREATE TABLE users (id int, username text, password text)"
+create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
+
 cursor.execute(create_table)
 
-user = (1, "javier", "123")
-insert_query = "INSERT INTO users VALUES (?, ?, ?)"
-cursor.execute(insert_query, user)
-
-users = [
-    (2, 'pepe', '123'),
-    (3, 'paco', '123')
-]
-
-cursor.executemany(insert_query, users)
-
-select_query = "SELECT * FROM users"
-for row in cursor.execute(select_query):
-    print(row)
-
 connection.commit()
-
 connection.close()
--- user.py
+++ user.py
@@ -1,4 +1,5 @@
 import sqlite3
+from flask_restful import Resource, reqparse
 
 
 class User:
@@ -34,3 +35,33 @@ class User:
             user = None
         connection.close()
         return user
+
+
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'username',
+        type=str,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+    parser.add_argument(
+        'password',
+        type=str,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+        if User.find_by_username(data['username']):
+            return {"message": "A user with that username already exists"}, 400
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "INSERT INTO users VALUES (NULL, ?, ?)"
+        new_user = (data['username'], data['password'])
+        cursor.execute(query, new_user)
+        connection.commit()
+        connection.close()
+
+        return {"message": "User created successfully"}, 201
